7517bcf9baec851365a48e46997ac2e54c69e129
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2009-08-07 09:44:15 +0000

    Added close menu option Revamped some strings

--- trunk/src/com/sputnik/wispr/AndroidWISPr.java
+++ trunk/src/com/sputnik/wispr/AndroidWISPr.java
@@ -2,11 +2,37 @@ package com.sputnik.wispr;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AndroidWISPr extends PreferenceActivity {
+	public static final int CLOSE_ID = Menu.FIRST;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.layout.preferences);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		boolean result = super.onCreateOptionsMenu(menu);
+		menu.add(0, CLOSE_ID, 0, R.string.menu_close).setIcon(android.R.drawable.ic_menu_close_clear_cancel);
+
+		return result;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean res = true;
+		switch (item.getItemId()) {
+			case CLOSE_ID:
+				finish();
+				break;
+			default:
+				res = super.onOptionsItemSelected(item);
+		}
+
+		return res;
 	}
 }
