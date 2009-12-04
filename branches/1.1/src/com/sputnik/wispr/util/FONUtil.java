5524ba2d234607124659b955fe4b31a50c1d0520
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2009-12-04 21:58:19 +0000

    Creando  branch para release 1.1

---
+++ branches/1.1/src/com/sputnik/wispr/util/FONUtil.java
@@ -0,0 +1,15 @@
+package com.sputnik.wispr.util;
+
+public class FONUtil {
+	public static boolean isFonNetWork(String ssid, String bssid) {
+		boolean res = false;
+		if (ssid != null) {
+			ssid = ssid.toUpperCase();
+			res = ssid.startsWith("FON_") || ssid.equals("NEUF WIFI FON");
+			if (!res && ssid.equals("BTFON")) {
+				res = bssid.startsWith("00:18:84");
+			}
+		}
+		return res;
+	}
+}
