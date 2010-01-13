7ecbce6c27402c8d8f45ed492c04221edc0bf8f4
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-01-13 13:14:48 +0000

    New Branch for  solving v1.3 bugs.

---
+++ branches/1.3.1/src/com/sputnik/wispr/NotificationCleaningService.java
@@ -0,0 +1,31 @@
+package com.sputnik.wispr;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class NotificationCleaningService extends IntentService {
+
+	private static String TAG = NotificationCleaningService.class.getName();
+
+	public NotificationCleaningService() {
+		super(TAG);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Log.d(TAG, "Handling disconection intent: " + intent);
+		if (intent.getAction().equals("CLEAN")) {
+			cleanNotification(this, intent);
+		}
+	}
+
+	private void cleanNotification(Context context, Intent intent) {
+		NotificationManager notificationManager = (NotificationManager) this
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		notificationManager.cancel(1);
+	}
+}
---
+++ branches/1.3.1/src/com/sputnik/wispr/util/HttpUtils.java
@@ -0,0 +1,84 @@
+package com.sputnik.wispr.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+
+public class HttpUtils {
+	static HttpParams defaultHttpParams = new BasicHttpParams();
+
+	static {
+		defaultHttpParams.setParameter(CoreProtocolPNames.USER_AGENT, "wispr");
+	}
+
+	public static String getUrl(String url) throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DefaultHttpClient httpclient = new DefaultHttpClient(defaultHttpParams);
+		httpclient.setCookieStore(null);
+		HttpGet httpget = new HttpGet(url);
+		HttpEntity entity = httpclient.execute(httpget).getEntity();
+		if (entity != null) {
+			entity.writeTo(baos);
+		}
+
+		String result = baos.toString().trim();
+		baos.close();
+
+		return result;
+	}
+
+	public static String getUrlByPost(String url, Map<String, String> params) throws IOException {
+		return getUrlByPost(url, params, null);
+	}
+
+	public static String getUrlByPost(String url, Map<String, String> params, Map<String, String> headers)
+			throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DefaultHttpClient httpclient = new DefaultHttpClient(defaultHttpParams);
+		httpclient.setCookieStore(null);
+
+		List<NameValuePair> formParams = new ArrayList<NameValuePair>();
+		if (params != null) {
+			Set<Entry<String, String>> paramsSet = params.entrySet();
+			for (Entry<String, String> entry : paramsSet) {
+				formParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+			}
+		}
+
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formParams, "UTF-8");
+		HttpPost httppost = new HttpPost(url);
+		httppost.setEntity(entity);
+
+		if (headers != null) {
+			Set<Entry<String, String>> headersSet = headers.entrySet();
+			for (Entry<String, String> entry : headersSet) {
+				httppost.setHeader(entry.getKey(), entry.getValue());
+			}
+		}
+
+		HttpEntity responseEntity = httpclient.execute(httppost).getEntity();
+		if (responseEntity != null) {
+			responseEntity.writeTo(baos);
+		}
+
+		String result = baos.toString().trim();
+		baos.close();
+
+		return result;
+	}
+}
