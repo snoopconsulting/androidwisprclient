d8b708a39e13b92ce29853499709b40ef5fdfad7
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-07-02 14:19:04 +0000

    Creating new branch for next release

---
+++ branches/1.4.5/src/com/sputnik/wispr/NotificationCleaningService.java
@@ -0,0 +1,33 @@
+package com.sputnik.wispr;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class NotificationCleaningService extends IntentService {
+
+	private static String TAG = NotificationCleaningService.class.getName();
+
+	public static final String ACTION_CLEAN = "CLEAN";
+
+	public NotificationCleaningService() {
+		super(TAG);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Log.v(TAG, "Handling disconection intent: " + intent);
+		if (intent.getAction().equals(ACTION_CLEAN)) {
+			cleanNotification(this, intent);
+		}
+	}
+
+	private void cleanNotification(Context context, Intent intent) {
+		NotificationManager notificationManager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		notificationManager.cancel(1);
+	}
+}
