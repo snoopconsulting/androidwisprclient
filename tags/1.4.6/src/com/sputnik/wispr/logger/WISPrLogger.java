c6e0060051327c0f8cd1a1048b9829da59b5e386
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-08-12 10:20:49 +0000

    New tag for 1.4.6 Release

---
+++ tags/1.4.6/src/com/sputnik/wispr/logger/WISPrLogger.java
@@ -0,0 +1,96 @@
+package com.sputnik.wispr.logger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import android.util.Log;
+
+import com.sputnik.wispr.handler.WISPrInfoHandler;
+import com.sputnik.wispr.handler.WISPrResponseHandler;
+import com.sputnik.wispr.util.FONUtil;
+import com.sputnik.wispr.util.HttpUtils;
+import com.sputnik.wispr.util.WISPrConstants;
+import com.sputnik.wispr.util.WISPrUtil;
+
+public class WISPrLogger implements WebLogger {
+
+	private static String TAG = WISPrLogger.class.getName();
+
+	protected String userParam = "UserName";
+
+	protected String passwordParam = "Password";
+
+	public String login(String user, String password) {
+		String res = WISPrConstants.WISPR_RESPONSE_CODE_INTERNAL_ERROR;
+		try {
+			String blockedUrlText = HttpUtils.getUrl(BLOCKED_URL);
+			if (!blockedUrlText.equalsIgnoreCase(CONNECTED)) {
+				String WISPrXML = WISPrUtil.getWISPrXML(blockedUrlText);
+				if (WISPrXML != null) {
+					// Log.d(TAG, "XML Found:" + WISPrXML);
+					WISPrInfoHandler wisprInfo = new WISPrInfoHandler();
+					android.util.Xml.parse(WISPrXML, wisprInfo);
+
+					if (wisprInfo.getMessageType().equals(WISPrConstants.WISPR_MESSAGE_TYPE_INITIAL)
+							&& wisprInfo.getResponseCode().equals(WISPrConstants.WISPR_RESPONSE_CODE_NO_ERROR)) {
+						res = tryToLogin(user, password, wisprInfo);
+					}
+				} else {
+					// Log.d(TAG, "XML NOT FOUND : " + blockedUrlText);
+					res = WISPrConstants.WISPR_NOT_PRESENT;
+				}
+			} else {
+				res = WISPrConstants.ALREADY_CONNECTED;
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "Error trying to log", e);
+			res = WISPrConstants.WISPR_RESPONSE_CODE_INTERNAL_ERROR;
+		}
+		// Log.d(TAG, "WISPR Login Result: " + res);
+
+		return res;
+	}
+
+	private String tryToLogin(String user, String password, WISPrInfoHandler wisprInfo) throws IOException,
+			ParserConfigurationException, FactoryConfigurationError {
+		String res = WISPrConstants.WISPR_RESPONSE_CODE_INTERNAL_ERROR;
+		String targetURL = wisprInfo.getLoginURL();
+		Log.d(TAG, "Trying to Log " + targetURL);
+		Map<String, String> data = new HashMap<String, String>();
+		data.put(userParam, user);
+		data.put(passwordParam, password);
+
+		String htmlResponse = HttpUtils.getUrlByPost(targetURL, data);
+		// Log.d(TAG, "WISPR Reponse:" + htmlResponse);
+		if (htmlResponse != null) {
+			String response = WISPrUtil.getWISPrXML(htmlResponse);
+			if (response != null) {
+				// Log.d(TAG, "WISPr response:" + response);
+				WISPrResponseHandler wrh = new WISPrResponseHandler();
+				try {
+					android.util.Xml.parse(response, wrh);
+					res = wrh.getResponseCode();
+				} catch (SAXException saxe) {
+					res = WISPrConstants.WISPR_NOT_PRESENT;
+				}
+			} else {
+				res = WISPrConstants.WISPR_NOT_PRESENT;
+			}
+		}
+
+		// If we dont find the WISPR Response or we cannot parse it, we check if we have connection
+		if (res.equals(WISPrConstants.WISPR_NOT_PRESENT)) {
+			if (FONUtil.haveConnection()) {
+				res = WISPrConstants.WISPR_RESPONSE_CODE_LOGIN_SUCCEEDED;
+			}
+		}
+
+		return res;
+	}
+}
