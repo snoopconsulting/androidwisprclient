4b33c0db7f83de6d8e2162187a24c205762c64e9
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-10-14 07:45:43 +0000

    Preparing tag for new release

---
+++ tags/1.4.7/src/com/sputnik/wispr/logger/LivedoorLogger.java
@@ -0,0 +1,30 @@
+package com.sputnik.wispr.logger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LivedoorLogger extends SimpleHTTPLogger {
+	protected static String TAG = LivedoorLogger.class.getName();
+
+	private static final String NETWORK_SUFIX = "@fon";
+
+	public LivedoorLogger() {
+		super("https://vauth.lw.livedoor.com/fauth/index");
+	}
+
+	@Override
+	protected Map<String, String> getPostParameters(String user, String password) {
+		Map<String, String> loginParams = new HashMap<String, String>();
+		loginParams.put("sn", "009");
+		loginParams.put("original_url", BLOCKED_URL);
+		loginParams.put("name", user + NETWORK_SUFIX);
+		loginParams.put("password", password);
+
+		return loginParams;
+	}
+
+	@Override
+	protected String getLogOffUrl() {
+		return null;
+	}
+}
