c80b3ba3ae0ca11e8444eb51129977cf11f053eb
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-10-16 06:00:25 +0000

    - Solved issue with LiveDoor logger not working - Typo in log message

--- src/com/sputnik/wispr/logger/LivedoorLogger.java
+++ src/com/sputnik/wispr/logger/LivedoorLogger.java
@@ -1,7 +1,12 @@
 package com.sputnik.wispr.logger;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import android.util.Log;
+
+import com.sputnik.wispr.util.HttpUtils;
 
 public class LivedoorLogger extends SimpleHTTPLogger {
 	protected static String TAG = LivedoorLogger.class.getName();
@@ -15,12 +20,29 @@ public class LivedoorLogger extends SimpleHTTPLogger {
 	@Override
 	protected Map<String, String> getPostParameters(String user, String password) {
 		Map<String, String> loginParams = new HashMap<String, String>();
-		loginParams.put("sn", "009");
+		loginParams.put("sn", getSNParameter());
 		loginParams.put("original_url", BLOCKED_URL);
 		loginParams.put("name", user + NETWORK_SUFIX);
 		loginParams.put("password", password);
+		// Click coordinates on image button, really not needed
+		loginParams.put("x", "66");
+		loginParams.put("y", "15");
 
 		return loginParams;
+	}
+
+	private String getSNParameter() {
+		String sn = "001";
+		try {
+			String blockedUrlText = HttpUtils.getUrl(BLOCKED_URL);
+			int start = blockedUrlText.indexOf("name=\"sn\" value=\"") + 17;
+			int end = blockedUrlText.indexOf("\"", start);
+			sn = blockedUrlText.substring(start, end);
+		} catch (IOException e) {
+			Log.e(TAG, "Error getting SN", e);
+		}
+
+		return sn;
 	}
 
 	@Override
