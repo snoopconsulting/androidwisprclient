b82df420f821aee43f6e2e1f27ddc4afa0cd4120
joanfisbein <joanfisbein@3016a844-68ed-11de-ba0c-230037762ff9>
2010-11-29 16:35:44 +0000

    Refactoring Utils classes

--- src/com/sputnik/wispr/util/FONUtils.java
+++ src/com/sputnik/wispr/util/FONUtils.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import com.sputnik.wispr.logger.WebLogger;
 
-public class FONUtil {
+public class FONUtils {
 	private static final String FON_MAC_PREFIX = "00:18:84";
 
 	public static boolean isSupportedNetwork(String ssid, String bssid) {
@@ -19,33 +19,33 @@ public class FONUtil {
 	}
 
 	public static boolean isNeufBox(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && (ssid.equalsIgnoreCase("NEUF WIFI FON") || ssid.equalsIgnoreCase("SFR WIFI FON"));
 	}
 
 	public static boolean isFonera(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && ssid.toUpperCase().startsWith("FON_") && !isLivedoor(ssid, bssid);
 	}
 
 	public static boolean isSBPublicFonera(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && ssid.equalsIgnoreCase("FON");
 	}
 
 	public static boolean isBtFonera(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && bssid != null && ssid.equalsIgnoreCase("BTFON") && bssid.startsWith(FON_MAC_PREFIX);
 	}
 
 	public static boolean isLivedoor(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && bssid != null && ssid.equalsIgnoreCase("FON_livedoor")
 				&& !bssid.startsWith(FON_MAC_PREFIX);
 	}
 
 	public static boolean isBtHub(String ssid, String bssid) {
-		ssid = FONUtil.cleanSSID(ssid);
+		ssid = FONUtils.cleanSSID(ssid);
 		return ssid != null && bssid != null && ssid.equalsIgnoreCase("BTFON") && !bssid.startsWith(FON_MAC_PREFIX);
 	}
 
