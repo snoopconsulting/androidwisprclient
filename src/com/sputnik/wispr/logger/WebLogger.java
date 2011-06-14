e869926545042d9eb2d2b10abce6499509c4c8c6
joan <joan@fisbein.com>
2011-06-14 13:42:51 +0000

    Branch for version 1.2

--- src/com/sputnik/wispr/logger/WebLogger.java
+++
@@ -1,9 +0,0 @@
-package com.sputnik.wispr.logger;
-
-public interface WebLogger {
-	public static final String CONNECTED = "CONNECTED";
-
-	public static final String BLOCKED_URL = "http://ubuntuone.com/p/F6b/";
-
-	public LoggerResult login(String user, String password);
-}
