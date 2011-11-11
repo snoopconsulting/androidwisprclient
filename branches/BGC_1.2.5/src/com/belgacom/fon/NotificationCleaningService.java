efefa1a14266622061e342974c5f4cce247d8732
joan@fisbein.com <joan@fisbein.com@3016a844-68ed-11de-ba0c-230037762ff9>
2011-11-11 16:39:04 +0000

    Starting rename process

---
+++ branches/BGC_1.2.5/src/com/belgacom/fon/NotificationCleaningService.java
@@ -0,0 +1,41 @@
+package com.belgacom.fon;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.belgacom.fon.util.Utils;
+import com.oakley.fon.R;
+
+public class NotificationCleaningService extends IntentService {
+
+	private static String TAG = NotificationCleaningService.class.getName();
+
+	public static final String ACTION_CLEAN = "CLEAN";
+
+	public NotificationCleaningService() {
+		super(TAG);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Log.v(TAG, "Handling disconection intent: " + intent);
+		if (intent.getAction().equals(ACTION_CLEAN)) {
+			cleanNotification(this, intent);
+			cleanLogOffUrl(this, intent);
+		}
+	}
+
+	private void cleanLogOffUrl(Context context, Intent intent) {
+		Utils.removePreference(context, R.string.pref_logOffUrl);
+	}
+
+	private void cleanNotification(Context context, Intent intent) {
+		NotificationManager notificationManager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		notificationManager.cancel(1);
+	}
+}
