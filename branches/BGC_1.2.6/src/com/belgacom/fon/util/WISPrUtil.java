5aac5c33e8f6cdaa6e28a1795a3080e4a013c5eb
joan@fisbein.com <joan@fisbein.com@3016a844-68ed-11de-ba0c-230037762ff9>
2012-01-11 17:25:47 +0000

    Clean Code to remove "Magic Number"

--- branches/BGC_1.2.6/src/com/belgacom/fon/util/WISPrUtil.java
+++ branches/BGC_1.2.6/src/com/belgacom/fon/util/WISPrUtil.java
@@ -3,10 +3,14 @@ package com.belgacom.fon.util;
 public class WISPrUtil {
 	public static final String WISPR_TAG_NAME = "WISPAccessGatewayParam";
 
+	public static final String WISPR_TAG_START = "<" + WISPR_TAG_NAME;
+
+	public static final String WISPR_TAG_END = "</" + WISPR_TAG_NAME + ">";
+
 	public static String getWISPrXML(String source) {
 		String res = null;
-		int start = source.indexOf("<" + WISPR_TAG_NAME);
-		int end = source.indexOf("</" + WISPR_TAG_NAME + ">", start) + WISPR_TAG_NAME.length() + 3;
+		int start = source.indexOf(WISPR_TAG_START);
+		int end = source.indexOf(WISPR_TAG_END, start) + WISPR_TAG_END.length();
 		if (start > -1 && end > -1) {
 			res = new String(source.substring(start, end));
 			if (!res.contains("&amp;")) {
